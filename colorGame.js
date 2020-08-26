8854cc9341b2607e18c1c83a4050c8dd63288ffc
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-26 10:33:17 +0600

    Added reset button
    Added a reset button (name "New Color" or "Play Again" depending on whether the game has begun or not). Players no longer have to refresh the page to play the game again.

--- colorGame.js
+++ colorGame.js
@@ -5,6 +5,24 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	// Generate Random Colors
+	colors = generateRandomColors(6);
+	// Pick a new random color from array
+	pickedColor = pickColor();
+	// Change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	// Change color of squares
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	// Change color of h1 background
+	h1.style.backgroundColor = "#232323";
+	// Change reset button back to "New Colors"
+	resetButton.textContent = "New Colors";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -12,7 +30,7 @@ for (var i = 0; i < squares.length; i++) {
 	// Add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
 
-	// Add "clicl listeners" to squares
+	// Add "click listeners" to squares
 	squares[i].addEventListener("click", function(){
 		// Grab color of picked square
 		let clickedColor = this.style.backgroundColor;
@@ -21,6 +39,7 @@ for (var i = 0; i < squares.length; i++) {
 			messageDisplay.textContent = "Correct";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
+			resetButton.textContent = "Play Again";
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
