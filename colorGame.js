1e880a76382c198ed359ffceb7f0735a70dc5b71
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-26 14:34:47 +0600

    Refactored and Styled middle text
    Refactored the JS code to separate code longer than 10 lines into separate functions, to create a cleaner file. Also styled the font that shows if the selection is correct or wrong to fit with the other buttons around its sides.

--- colorGame.js
+++ colorGame.js
@@ -1,50 +1,18 @@
 let numSquares = 6;
-let colors = generateRandomColors(numSquares);
-
+let colors = [];
+let pickedColor;
 let squares = document.querySelectorAll(".square");
-let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
 let modeButtons = document.querySelectorAll(".mode");
 
-for(let i=0; i < modeButtons.length; i++){
-	modeButtons[i].addEventListener("click", function(){
-		modeButtons[0].classList.remove("selected");
-		modeButtons[1].classList.remove("selected");
-		this.classList.add("selected");
-		this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-		reset();
-	});
-}
+init();
 
 resetButton.addEventListener("click", function(){
 	reset();
 })
-
-colorDisplay.textContent = pickedColor;
-
-for (var i = 0; i < squares.length; i++) {
-	// Add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
-
-	// Add "click listeners" to squares
-	squares[i].addEventListener("click", function(){
-		// Grab color of picked square
-		let clickedColor = this.style.backgroundColor;
-		// compare color to pickedColor
-		if (clickedColor === pickedColor) {
-			messageDisplay.textContent = "Correct";
-			changeColors(clickedColor);
-			h1.style.backgroundColor = clickedColor;
-			resetButton.textContent = "Play Again";
-		} else {
-			this.style.backgroundColor = "#232323";
-			messageDisplay.textContent = "Try Again";
-		}
-	})
-}
 
 function changeColors(color){
 	// Loop through all squares
@@ -101,4 +69,42 @@ function reset(){
 	}
 	// Change color of h1 background
 	h1.style.backgroundColor = "steelblue";
+}
+
+function init(){
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons(){
+	for(let i=0; i < modeButtons.length; i++){
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			reset();
+		});
+	}
+}
+
+function setupSquares(){
+	for (var i = 0; i < squares.length; i++) {
+		// Add "click listeners" to squares
+		squares[i].addEventListener("click", function(){
+			// Grab color of picked square
+			let clickedColor = this.style.backgroundColor;
+			// compare color to pickedColor
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct";
+				changeColors(clickedColor);
+				h1.style.backgroundColor = clickedColor;
+				resetButton.textContent = "Play Again";
+			} else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+		})
+	}
 }
